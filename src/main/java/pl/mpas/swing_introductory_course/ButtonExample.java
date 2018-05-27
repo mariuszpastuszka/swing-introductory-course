7dc152729508631edfb5dfec86ce873caa669eb5
mariuszpastuszka <mpastuszka@gmail.com>
2018-05-27 12:59:59 +0200

    [add] - some swing examples

---
+++ src/main/java/pl/mpas/swing_introductory_course/ButtonExample.java
@@ -0,0 +1,15 @@
+package pl.mpas.swing_introductory_course;
+
+import javax.swing.*;
+
+public class ButtonExample {
+    public static void main(String[] args) {
+        JFrame f = new JFrame("Button Example");
+        JButton b = new JButton("Click Here");
+        b.setBounds(50, 100, 95, 30);
+        f.add(b);
+        f.setSize(400, 400);
+        f.setLayout(null);
+        f.setVisible(true);
+    }
+}
---
+++ src/main/java/pl/mpas/swing_introductory_course/TextAreaExample.java
@@ -0,0 +1,42 @@
+package pl.mpas.swing_introductory_course;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TextAreaExample implements ActionListener {
+    JLabel l1, l2;
+    JTextArea area;
+    JButton b;
+
+    TextAreaExample() {
+        JFrame f = new JFrame();
+        l1 = new JLabel();
+        l1.setBounds(50, 25, 100, 30);
+        l2 = new JLabel();
+        l2.setBounds(160, 25, 100, 30);
+        area = new JTextArea();
+        area.setBounds(20, 75, 250, 200);
+        b = new JButton("Count Words");
+        b.setBounds(100, 300, 120, 30);
+        b.addActionListener(this);
+        f.add(l1);
+        f.add(l2);
+        f.add(area);
+        f.add(b);
+        f.setSize(450, 450);
+        f.setLayout(null);
+        f.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new TextAreaExample();
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        String text = area.getText();
+        String words[] = text.split("\\s");
+        l1.setText("Words: " + words.length);
+        l2.setText("Characters: " + text.length());
+    }
+}
